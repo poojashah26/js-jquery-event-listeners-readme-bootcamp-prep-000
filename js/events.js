71cd4c3f33dee1e0e97abd0243db5d659cd2917f
Pooja Shah <pooja_shah26@hotmail.co.uk>
2019-08-18 15:57:51 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+  });
+}
 
-$(document).ready(function(){
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  });
+}
 
-// call functions here
+function pressIt(){
+  $("input").on("keydown", function(key){
+    if (key.which==71){
+      alert('G was pressed');
+    }
+  });
+}
 
-});
+function submitIt(){
+  $("form").on("submit", function(){
+    alert('Your form is going to be submitted now.');
+  });
+}
+
+$(document). ready(function(){
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
+})
